a3983b1e5b0c133652752d95ca8a7f313b6107a3
Barry Smart <barry.smart@endjin.com>
2021-02-08 15:41:36 +0000

    Experimentation with GitHub workflow.

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="project_name",
     version="0.0.1",
     author="Example Author",
@@ -13,14 +12,12 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
-    install_requires=[
-        "numpy==1.19.4; python_version >= '3.6'",
-    ],
+    python_requires=">=3.8",
+    install_requires=["numpy==1.20.1"],
 )
